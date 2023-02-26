a19958d43cffb810448bdcd780b169768702d51c
Abhishek Duttagupta <abhidgpta@gmail.com>
2023-02-26 23:58:10 +0000

    update

--- Laplacianmatting.m
+++ Laplacianmatting.m
@@ -1,6 +1,6 @@
-clc
-clear all
-close
+
+function alpha = Laplacianmatting (img, trimap)
+
 % Laplace matting first uses user-supplied foreground and background 
 % markers (trimap) to determine the set of foreground and background 
 % pixels in an image, and then uses a Laplace matrix to encode the 
@@ -14,6 +14,7 @@ close
 % and background labels to determine which pixels belong to the foreground and
 % which pixels belong to the background. These markers are usually done by manually 
 % drawing foreground and background boundaries. That is the trimap.png image.
+
 % Step 2: Construct the Laplace matrix After labeling the foreground and background pixels, 
 % construct the Laplace matrix reflecting the difference between each pixel and its neighbors 
 % in the image. The place where the "difference" is large is likely to be the junction of the 
@@ -24,26 +25,13 @@ close
 % value of each pixel, indicating the opacity of each pixel in the final matte. Finally, the 
 % alpha is "covered" on the original image to complete the cutout.
 
+
 % The input image is a color image in rgb format; the trimap is a grayscale image
-
 % Load the input image and trimap
-
 %function [img,trimap] = laplace(img,trimap)
-
-img = imread('input.jpg');      % Read input images
-trimap = imread('trimap.png');  % Read trimatte images
-
-
-% Create the Laplacian matrix
-
-falpha = laplacian(img,trimap);
-
-function alpha = laplacian (img,trimap)
-
 % Convert the image and trimap to double precision and normalize
-img = double(img); % Raw read in uint8 image data to double data (integer to floating point)
-trimap = double(trimap);             
-trimap = trimap ./ 255;% Normalization process as the original image is 8bit.2^8=256
+img = im2double(img); % Raw read in uint8 image data to double data (integer to floating point)
+trimap = im2double(trimap);             
 
 % Get the size of the image
 [m, n, c] = size(img); % m - number of pixels in the x direction, n - number of pixels in the y direction, c - rgb tricolour, number of channels is 3    
@@ -54,30 +42,23 @@ trimap = trimap ./ 255;% Normalization process as the original image is 8bit.2^8
 % are judged to be foreground, the parts with a grey level less than a certain threshold 
 % (very "black") are judged to be background, and the other parts are to be confirmed. 
 % The part to be confirmed is further processed to determine whether it is the foreground or the background
-fg = trimap > 0.70;             % fg is a matrix as large as the image, 
+fg = trimap > 0.90;             % fg is a matrix as large as the image, 
                                 % with an element of 1 indicating that the point is the foreground
-bg = trimap < 0.01;             % In bg, an element of 1 means that the point is background
+bg = trimap < 0.10;             % In bg, an element of 1 means that the point is background
 unk = ~(fg | bg);               % In unk, an element of 1 means neither foreground nor background, to be confirmed
-
-
-
-
 
 Laplacian = del2(img);          % Calculating the Laplace matrix
 
 % Calculate the alpha matte
 alpha = zeros(m, n);            % Initialize alpha matrix
-for i = 1:c                     % Calculating the alpha matrix
-%     alpha(unk(:, :, 1)) = alpha(unk(:, :, 1)) + Laplacian(unk(:, :, 1), i) .^ 2;
-%     alpha(unk) = alpha(unk) + Laplacian(unk(:,:), i) .^ 2;
-    subLaplacian = Laplacian(:,:,i);
-    alpha(unk) = alpha(unk) + subLaplacian(unk).^2;
+
+for i = 1 : c                     % Calculating the alpha matrix
+    subLaplacian = Laplacian(:, :, i);
+    alpha(unk) = alpha(unk) + subLaplacian(unk) .^ 2;
 end
+
 alpha = 1 - sqrt(alpha ./ c);
 alpha(bg(:, :, 1)) = 0;         % Forced repositioning of sections already identified as background
 alpha(fg(:, :, 1)) = 1;        
-
-% Save the output alpha matte
-imwrite(alpha, 'output_alpha.png');     % Export alpha for keying
 
 end
