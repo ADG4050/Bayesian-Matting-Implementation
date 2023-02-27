4d4b7d1ec7da432240ae878f8b82ca5dd9381f66
Abhishek Duttagupta <aduttagu@tcd.ie>
2023-02-27 18:31:22 +0000

    fix merge conflicts

--- main.m
+++ main.m
@@ -25,13 +25,13 @@ GT = imread("C:\Users\aduttagu\Downloads\gt_training_lowres\GT19.png");
 % Converting the images to double format, as all matrix computations are
 % not possible in unsigned integer 8 format, also double represents (0-255)
 % in scale of (0-1).
-im = im2double(image);
+image = im2double(image);
 trimap = im2double(trimap);
 GT = im2double(GT);
 
 % Step 4
 % Initializing all parameters required for the complete process.
-N = 100; % Neighbourhood window size
+N = 25; % Neighbourhood window size
 sigma = 8; % standard deviation of the Gaussian distribution
 % clustering parameter
 minN = 10; % minimum weights required for clustering function activation 
@@ -49,26 +49,22 @@ minLike=  1e-6; % minimal change in likelihood between consecutive iterations
 % region. 
 bg_reg = (trimap == 0); % background 
 fg_reg = (trimap == 1); % foreground 
-unkmask = (~ bg_reg & ~ fg_reg); % unknow region 
-
-
+un_reg = (~ bg_reg & ~ fg_reg); % unknow region 
 
 % Step 5.2
 % Making three dimentional matrix (r, g, b), so that the seperate logical 
 % matrices made before are applicable for the foreground and background
 % matrices. 
-F = im;
-B = im;
+F = image;
+B = image;
 FG = repmat(~fg_reg, [1, 1, 3]);
 BG = repmat(~bg_reg, [1, 1, 3]);
-
 
 % Step 5.3
 % with help of FG and BG, separating foreground and background regions from
 % the image
 F(FG) = 0; % putting all values except FG = 0
 B(BG) = 0; % putting all values except BG = 0
-
 % figure(2);
 % subplot(1, 2, 1);
 % imshow(F);
@@ -82,8 +78,8 @@ B(BG) = 0; % putting all values except BG = 0
 % and calculating the number of unknown region cells as Unknown_sum.
 alpha = zeros(size(trimap));
 alpha(fg_reg) = 1;
-alpha(unkmask) = NaN;
-Unknown_sum = sum(unkmask(:));
+alpha(un_reg) = NaN;
+Unknown_sum = sum(un_reg(:));
 
 % Step 5.5
 % As per the bayesian matting paper, guassian falloff is used for 
@@ -100,41 +96,32 @@ g = g / max(g(:)); %normalizing it.
 % at borders.
 sq = strel('square', 3);
 
-unreg = unkmask; %transferring data to prevent data loss and inappropiate use.
+unreg = un_reg; %transferring data to prevent data loss and inappropiate use.
 n = 1; % while loop initial n parameter.
-
-% Creating a counter to issue warning, once max iteration has reached and 
-% stopping the iterative loop. Condition = max iteration described below.
-m_i = 0;
-max_li = 20;
 
 % Step 5.7
 % Creating while loop with all unknown region cells marked NaN and 
 % calculating F,B, alpha using an iterative process. 
+cc = 0;
 
 while n < Unknown_sum
 
     % Step 5.7.1 certain pixels are removed from the unknown region of the
     % image to detect the edges of the image.
     unreg = imerode(unreg, sq);
-    edge_pixels = ~unreg & unkmask;
+    edge_pixels = ~unreg & un_reg;
 
     % Step 5.7.2 finding the position of the pixels inside the above
     % unknown boundary region
-    [Y, X] = find(edge_pixels);
-
-    % Step 5.7.2* Creating a counter to issue warning, once max iteration
-    % has reached and stopping the iterative loop.
-    % Condition = max iteration described below.
-    max_rep = 0; 
-
+    [Y, X] = find(edge_pixels); 
+    aa = 0; bb = 0; 
     % Step 5.7.3 Creating a for loop inside the unknown region for
     % iterating along the count of Y that we just computed.
     for i = 1 : length(Y)
         % Step 5.7.3.1 taking the pixel and reshaping its 3 channel value 
         % into a 3 X 1 matrix
         x = X(i); y = Y(i);
-        chan = reshape(im(y, x, :), [3, 1]); 
+        chan = reshape(image(y, x, :), [3, 1]); 
 
         % Step 5.7.3.2 taking the surrounding pixel values around that 
         % pixel. (Used calcsurr_alpha Function to calaculate surrounding 
@@ -168,25 +155,13 @@ while n < Unknown_sum
 
         % Step 5.7.3.5 Clustering function will be done only weights of F &
         % B are more than 10 or else we go to next iteration without
-        % clustering.
+        % clustering
         if length(wghts_f) < minN || length(wghts_b) < minN
-            max_rep = max_rep + 1; 
-
-        % Step 5.7.3.5* If whole iteration in the for loop runs without
-        % getting enough background or foreground values for some time, 
-        % means the loop has become infinite as no more suitable window to 
-        % calculate F,B and alpha, hence condition to break the loop and display the result
-        % accordingly.
-            if (max_rep == length(Y))
-                m_i = m_i + 1;
-                if (m_i == max_li)
-                    n = Unknown_sum;
-                    disp("warning : Max iteration for applying MAP with " + ...
-                        "the user set window has reached");
-                end
-            end
+            aa = aa + 1; 
+%           disp('skipping');
             continue;
         end
+%         disp("continued");
         
         % Step 5.7.3.6 Partitioning the foreground and background pixels 
         % (in a weighted manner with a clustering function) and calculate
@@ -217,16 +192,28 @@ while n < Unknown_sum
 
         % Step 5.7.3.10 - removing from unkowns for the next iteration. 
         unkmask(y,x) = 0;  
+
+
+        bb = bb + 1;
         n = n + 1;
     end
     
+%     if (n < 30000)
+%         disp('yay');
+%         cc = cc + 1;
+%         disp(cc);
+%     
+%     end
+
 end
+
+
 
 % Step 6
 % Displaying the alpha matte created from the given image and trimap
 figure(3);
 subplot(1, 3, 1);
-imshow(im);
+imshow(image);
 title('Input Image');
 subplot(1, 3, 2);
 imshow(trimap);
