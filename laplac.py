52d4f284c6ec39b214733adb5ee75d5c2b524979
zhuy3 <117815839+zhuy3@users.noreply.github.com>
2023-03-31 19:55:35 +0100

    Add files via upload

--- laplac.py
+++ laplac.py
@@ -1,46 +1,44 @@
 import numpy as np
-from skimage.color import rgb2gray
-from skimage.filters import laplace
 import cv2
+from matplotlib import pyplot as plt
 
-img_path = 'C:/Users/zhuy3/Desktop/Bayesian-Matting-Implementation/input.jpg'
-trimap_path = 'C:/Users/zhuy3/Desktop/Bayesian-Matting-Implementatio/trimap.png'
+img_path = 'C:/Users/zhuy3/Documents/La/input.jpg'
+trimap_path = 'C:/Users/zhuy3/Documents/La/trimap.png'
 
 img = cv2.imread(img_path)
 trimap = cv2.imread(trimap_path)
-
-#output = Laplacianmatting(image_path, trimap_path)
 
 def Laplacianmatting(img, trimap):
     # Convert img and trimap to double
     img = img.astype(np.float64) / 255.0
     trimap = trimap.astype(np.float64) / 255.0
-    
+
     # Get the size of the image
     m, n, c = img.shape
-    
-    # Calculate the foreground, background, and unknown pixels
+   
     fg = trimap > 0.9
     bg = trimap < 0.1
     unk = ~(fg | bg)
-    
+   
     # Calculate the Laplacian matrix
-    laplacian = laplace(rgb2gray(img))
-    
+    Laplacian = cv2.Laplacian(img, cv2.CV_64F)
+   
     # Calculate the alpha matte
     alpha = np.zeros((m, n))
-    
+   
     for i in range(c):
-        subLaplacian = laplacian[:,:,i]
-        alpha[unk] += subLaplacian[unk] ** 2
-    
-    alpha = 1 - np.sqrt(alpha / c)
+        subLaplacian = Laplacian[:,:,i]
+        alpha[unk[:,:,0]] += subLaplacian[unk[:,:,0]]**2
+   
+    alpha = 1 - np.sqrt(alpha/c)
     alpha[bg[:,:,0]] = 0
     alpha[fg[:,:,0]] = 1
-    
+   
     return alpha
 
+alpha = Laplacianmatting(img, trimap)
+plt.imshow(alpha, cmap='gray')
+plt.show()
 
-#result =type(img)
 
-#print(result)
+
