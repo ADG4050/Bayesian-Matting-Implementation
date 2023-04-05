b0214a50d304bf584f16e67461af23ed605f371c
Jiechai <117815957+Jiechai@users.noreply.github.com>
2023-04-05 19:41:18 +0100

    final version

--- unit _test.py
+++ unit _test.py
@@ -1,85 +1,78 @@
-#Please install the following library:
-import numpy as np
-import math
-import cv2
 import unittest
+from PIL import Image, ImageOps
+from Baysian_Mat import Bayesian_Matte
+import numpy as np
+from compositing import compositing
+from numpy.testing import assert_array_equal
+from Baysian_Mat import calcsurr_alpha
+from orchard_bouman_clust import clustFunc
 
-# please change the alpha path to ensure the code runs
-path_alpha = r'C:\Users\sunshine\Desktop\code\PythonFiles\computationLab\GT01.png'
 
-# please change the ground turth alpha path to ensure the code runs
-path_GTalpha = r'C:\Users\sunshine\Desktop\code\PythonFiles\computationLab\GT01.png'
+image = np.array(Image.open("C:/Users/aduttagu/Desktop/Bayesian-Matting-Implementation/input_training_lowres/GT06.png"))
+image_trimap = np.array(ImageOps.grayscale(Image.open("C:/Users/aduttagu/Desktop/Bayesian-Matting-Implementation/trimap_training_lowres/Trimap1/GT06.png")))
+background = np.array(Image.open('C:/Users/aduttagu/Desktop/Bayesian-Matting-Implementation/background.png'))
 
-# please change the inpur image path to ensure the code runs
-path_input = r'C:\Users\sunshine\Desktop\code\PythonFiles\computationLab\input.png'
 
-class TestAlpha(unittest.TestCase):
-    
-    # 1.This is the dimension check
+alpha,pixel_count = Bayesian_Matte(image,image_trimap) 
+alpha_disp = alpha * 255
+alpha_int8 = np.array(alpha,dtype = int)
+
+comp_Bay = compositing(image, alpha_disp, background)
+
+#unittest Codes
+class testcode(unittest.TestCase):
+    """ Unit test - 1 : Checks Input Image is correct or not"""
     def test_dimcheck(self):
-        # read input image 
-        image = cv2.imread(path_input)
         # check if it is a 3 channel image
         s3 = np.size(image,2)
         message = "Your input image must be 3 channels"     
         self.assertEqual(s3, 3, message) 
-       
-    # 2.This is the data type check   
-    def test_typecheck(self):
-        # read input image 
-        image = cv2.imread(path_input)
-        # check if the type is float or int
-        datatype= type(image)
-        t = (datatype=='int'or'float')
-        message="Data type should be int or float"
-        self.assertTrue(t,message)
-        
-        
-    # 3.This is the size check 
-    def test_sizecheck(self): 
-        # read alpha and ground truth
-        alpha=cv2.imread(path_alpha)
-        GTalpha=cv2.imread(path_GTalpha)
-        
-        # get  the number of row and column
-        row_alpha=np.size(alpha,0)
-        row_GT=np.size(GTalpha,0)
-        col_alpha=np.size(alpha,1)
-        col_GT=np.size(GTalpha,1)
-        
-        message1="The number of rows must be consistent."
-        message2="The number of columns must be consistent."
-        self.assertEqual(row_alpha, row_GT,message1)
-        self.assertEqual(col_alpha, col_GT,message2)    
-        
-    
-    # 4.This is the value check
-    def test_valuecheck(self):
-        # read alpha 
-        alpha=cv2.imread(path_alpha)
-        # check if any value is 0 or 1.
-        mark =0
-        if alpha.any() == 0|1:
-            mark=1   
-        message="all values in alpha should only be 0 or 1"                 
-        self.assertEqual(mark,1,message)
 
 
-if __name__=='__main__':
+    """ Unit test - 2 : Checks Input and Composite image shape"""
+    def test_image_dim(self):
+        # Get the shapes of the input image and composite image
+        Image1 = image.shape
+        Image2 = comp_Bay.shape
+
+        # Assert that the input image and composite image are equal
+        self.assertEqual(Image1, Image2, "The shapes of the input image and composite are not equal")
+
+
+    """ Unit test - 3 : Checks Input image and Alpha height and width"""
+    def test_image_size(self):
+        # Get the shapes of input image and alpha
+        h1,w1,c1 = image.shape
+        h2,w2 = alpha_disp.shape
+
+        # Assert that the sizes are equal
+        self.assertEqual(h1, h2, "Height of the input image and alpha matte are not equal")
+        self.assertEqual(w1, w2, "Width of the input image and alpha matte are not equal")
+
+
+    '''Unit test - 4 : Checks the window function is working or not.
+    A random 100x100x3 image is created. The get_window() function is called with the center coordinates (25, 25) and the default window size of 75. 
+    The expected output is a 75x75x3 sub-image of the input image centered at (25, 25). Again, the assert_array_equal() method is used to compare the 
+    shape of the output of the function with the expected shape. '''
+    def test_get_window(self):
+        m = np.random.rand(100, 100, 3)
+        window = calcsurr_alpha(m, 25, 25, 75)
+        window_shape = window.shape
+        expected_shape = (75, 75, 3)
+        assert_array_equal(window_shape, expected_shape)
+        
+
+    '''Unit test - 5 : Checks the functionality of orchard Bouman function.
+    Here we make random arrays S (100X3) and w (100X1) and pass it through the orchard Bouman function to confirm the shape and size of mean 
+    and covariance matrices, which is checked through an assert syntax. Answer should be 3 and 3X3'''
+    def test_clustFunc(self):
+        # Generate random data and weights
+        np.random.seed(1234)
+        S = np.random.randn(100, 3)
+        w = np.ones(100)
+        mu, sigma = clustFunc(S, w)
+        assert mu.shape[1] == 3
+        assert sigma.shape[1:] == (3, 3)
+
+if __name__ == '__main__':
     unittest.main()
-
-
-        
-
-
-
-    
-
-
-
-
-        
-
-
-
-    
