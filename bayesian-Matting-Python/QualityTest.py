71f4c88444c5a9b8609777f679d946805a8b02da
Abhishek Duttagupta <aduttagu@tcd.ie>
2023-04-06 10:47:20 +0100

    FINAL

--- bayesian-Matting-Python/QualityTest.py
+++ bayesian-Matting-Python/QualityTest.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 
+
 def mse2d(alpha, GT):
     """
     Calculate the Mean Squared Error (MSE) between two 2D images represented as numpy arrays.
@@ -8,16 +9,19 @@ def mse2d(alpha, GT):
     Parameters:
     alpha (numpy array): The first image to compare.
     GT (numpy array): The second image to compare.
-    
+
     Returns:
     float: The MSE value between the two images.
     """
     if np.isnan(np.max(alpha)):
-        msevalue = np.nansum(np.abs(alpha - GT)**2)/(alpha.shape[0]*alpha.shape[1])
+        msevalue = np.nansum(np.abs(alpha - GT)**2) / \
+            (alpha.shape[0]*alpha.shape[1])
     else:
-        msevalue = np.sum(np.abs(alpha - GT)**2)/(alpha.shape[0]*alpha.shape[1])
-    
+        msevalue = np.sum(np.abs(alpha - GT)**2) / \
+            (alpha.shape[0]*alpha.shape[1])
+
     return msevalue
+
 
 def sad2d(alpha, GT):
     """
@@ -26,7 +30,7 @@ def sad2d(alpha, GT):
     Parameters:
     alpha (numpy array): The first image to compare.
     GT (numpy array): The second image to compare.
-    
+
     Returns:
     float: The SAD value between the two images.
     """
@@ -35,9 +39,8 @@ def sad2d(alpha, GT):
     else:
         sadvalue = np.sum(np.abs(alpha - GT))
 
-    
-
     return sadvalue
+
 
 def psnr2d(alpha, GTalpha):
     """
@@ -46,17 +49,12 @@ def psnr2d(alpha, GTalpha):
     Parameters:
     alpha (numpy array): The first image to compare.
     GTalpha (numpy array): The second image to compare.
-    
+
     Returns:
     float: The PSNR value between the two images.
     """
 
     msevalue = mse2d(alpha, GTalpha)
-    max_pixel = 256
+    max_pixel = 255
     pnsrvalue = 10 * math.log10((max_pixel**2) / msevalue)
     return pnsrvalue
-
-
-
-
-
